f54a4a9345a86bea75bfa149bf15e38ebd44937d
hannaka <93572707+hannakausa@users.noreply.github.com>
2023-11-28 10:33:16 +0000

    Update TestDeepThought.java

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 public class TestDeepThought {
-    public TestDeepThought() {
+    public TestDeepThought() { 
 
     }
         @Test
